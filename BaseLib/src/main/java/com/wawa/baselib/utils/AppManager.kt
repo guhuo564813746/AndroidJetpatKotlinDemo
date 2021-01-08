93934d7b8463894d9070ba730031358305b112d7
zhangjin <Zhangjin@seafarer.me>
2021-01-08 18:50:20 +0800

    kotlin jetpat

---
+++ BaseLib/src/main/java/com/wawa/baselib/utils/AppManager.kt
@@ -0,0 +1,10 @@
+package com.wawa.baselib.utils
+
+/**
+ *作者：create by 张金 on 2021/1/6 11:37
+ *邮箱：564813746@qq.com
+ */
+object AppManager {
+    //单例模式
+
+}
---
+++ BaseLib/src/main/java/com/wawa/baselib/utils/Utils.kt
@@ -0,0 +1,14 @@
+package com.wawa.baselib.utils
+
+/**
+ *作者：create by 张金 on 2021/1/6 11:03
+ *邮箱：564813746@qq.com
+ */
+class Utils {
+    companion object{
+        @JvmStatic
+        fun doAction(){
+            doSomethings()
+        }
+    }
+}
